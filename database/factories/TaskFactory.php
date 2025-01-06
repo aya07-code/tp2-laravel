62356357c49418fa3ba0c2525a424de824441aa5
ayaziad <ayaziad387@gmail.com>
2025-01-06 01:05:44 +0100

    new folders

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    protected $model = \App\Models\Task::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'is_completed' => $this->faker->boolean,
+        ];
+    }
+}
+
